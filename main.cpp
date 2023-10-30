1ab3e789f39d03eb7ce58e8af433919954155c92
Egor <112934317+Forehm@users.noreply.github.com>
2023-10-30 23:50:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 
 string alphabet = "qwertyuiopasdfghjklzxcvbnm_,.!():?-+=1234567890%*QWERTYUIOPASDFGHJKLZXCVBNM"s;
-string key ="";
+string key =""s;
 vector<string> square;
 
 
