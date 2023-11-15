011aedc8a748d43984f97f262cd7b40646a75e3a
Egor <112934317+Forehm@users.noreply.github.com>
2023-11-15 23:04:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -129,7 +129,7 @@ void FillSquare(string alphabet_copy)
 
 int main()
 {
-	FillSquare(alphabet);//to start working, it is necessary
+	FillSquare(alphabet);//to start working, it is necessary to fill your 
 
 
 	return 0;
